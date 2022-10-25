93fd18f8ae375b220e291621b73a59e57d5a9ffa
Danya <daniilkhro.study@gmail.com>
2022-10-26 02:16:30 +0300

    Added components Categories, Header, PizzaCard, Sort

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import Header from "./src/components/Header";
+import Categories from "./src/components/Categories";
+import Sort from "./src/components/Sort";
+import PizzaCard from "./src/components/PizzaCard";
 function App() {
-   return (
-      <div className="App">
-         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-               Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-               className="App-link"
-               href="https://reactjs.org"
-               target="_blank"
-               rel="noopener noreferrer">
-               Learn React
-            </a>
-         </header>
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {/* {[...Array(8)].map((_, index) => (
+                <PizzaCard key={index}  />
+              ))} */}
+              <PizzaCard title={"Мексиканська піца"} price={900} />
+            </div>
+          </div>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
 
 export default App;
